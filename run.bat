9c53db1b9f0dd8eaba736b69ab76896e683237fd
SiniMini876 <yoavsinai22@gmail.com>
2021-08-07 01:38:06 +0300

    Updated changes to REPLIT

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-node src/index.js
+unset PREFIX
+
+export NVM_DIR="$HOME/.nvm" &&
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+
+nvm install 16.6.0 &&
+nvm use 16.6.0
+
+npm start
