b86340a9e6b1310f68ee308db250bdc1aad76fca
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2022-03-25 13:53:50 +0300

    Added rate limit

--- src/web/web.ts
+++ src/web/web.ts
@@ -2,11 +2,11 @@
 import express from "express";
 import path from "path";
 const app = express();
+import RateLimit from "express-rate-limit";
 let PORT = process.env.PORT as string | 4500;
 
 export const web = {
     execute: async () => {
-
         let dir = __dirname.slice(0, -8);
 
         let pathHtml = path.join(dir + "/src/web/index.html");
@@ -14,6 +14,14 @@ export const web = {
         let dirPath = path.join(dir + "/src/web/");
 
         app.use(express.static(dirPath));
+
+        const limiter = RateLimit({
+            windowMs: 1 * 60 * 1000, // 1 minute
+            max: 5,
+        });
+
+        // apply rate limiter to all requests
+        app.use(limiter);
         app.get("/", function (req, res) {
             res.sendFile(pathHtml);
         });
